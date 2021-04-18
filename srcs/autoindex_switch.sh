fc47d0d1771a14df1ea80f8f29a77da4d1a95729
mvaldes <mvaldes@student.42.fr>
2021-04-18 21:02:48 +0200

    adding feature to turn autoindex off and on

---
+++ srcs/autoindex_switch.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+if grep -c 'autoindex off' /etc/nginx/sites-available/ft_server; then
+    sed -i -r 's/autoindex off/autoindex on/' /etc/nginx/sites-available/ft_server
+else
+    sed -i -r 's/autoindex on/autoindex off/' /etc/nginx/sites-available/ft_server
+fi
+service nginx reload
